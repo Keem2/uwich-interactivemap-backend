de26cf84f43b7618427376290c67556c2dfcf843
Akeem Smith <asmith.339@outlook.com>
2024-11-26 11:27:18 -0400

    Initial commit

---
+++ src/main/java/com/uwich/interactivemap/InteractivemapApplication.java
@@ -0,0 +1,13 @@
+package com.uwich.interactivemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractivemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractivemapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uwich/interactivemap/InteractivemapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uwich.interactivemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractivemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
