60641285024ececa4550a9ec753864044ce0e8ef
Akeem Smith <asmith.339@outlook.com>
2025-01-07 08:46:18 -0400

    Implemented CORS

---
+++ src/main/java/com/uwich/interactivemap/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.uwich.interactivemap.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+// Global CORS config
+// allows all endpoints
+// with allowed endpoints being localhost:5173 (for vite)
+public class WebConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+        }
+}
